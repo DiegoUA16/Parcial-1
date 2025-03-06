e22f36a1c77c9cc3902e9b5ec07071f9affdbb0e
oneider16 <oneideralmendra5@gmail.com>
2025-03-06 10:02:25 -0500

    Subiendo Helper de Impresion.

--- src/main/java/Helpers/HelperImpresion.java
+++ src/main/java/Helpers/HelperImpresion.java
@@ -4,10 +4,25 @@
  */
 package Helpers;
 
-/**
- *
- * @author 1002821333
- */
+import Logica_Negocio.Persona;
+
+
+
+
+
 public class HelperImpresion {
-    
+    public static void ImprimirPersona (Persona objPersona){
+        
+        System.out.println("----------------------------------");
+        System.out.println("IMPRESION DE DATOS");
+        System.out.println("");
+        System.out.println("El nombre de la persona es"+"\t"+objPersona.getNombrePersona());
+        System.out.println("El apellido de la persona es"+"\t"+objPersona.getApellido());
+        System.out.println("La cedula de la persona es"+"\t"+objPersona.getIdPersona());
+        System.out.println("");
+        System.out.println("PRODUCTOS");
+        System.out.println("Id del Producto: "+"\t"+objPersona.getObjProducto().getId());
+        System.out.println("El Nombre del producto es: "+"\t"+objPersona.getObjProducto().getNombre());
+        System.out.println("Descripcion del producto: "+"\t"+objPersona.getObjProducto().getDescripcion());
+    }
 }
