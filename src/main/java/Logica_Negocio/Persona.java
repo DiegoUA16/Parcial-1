adb4011f27948e7ed134e81c31dc1bd182b94c3f
USER <USER@192.168.246.144>
2025-03-06 09:04:34 -0500

    Subiendo Clase Persona

--- src/main/java/Logica_Negocio/Persona.java
+++ src/main/java/Logica_Negocio/Persona.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Logica_Negocio;
 
 /**
@@ -9,5 +6,51 @@ package Logica_Negocio;
  * @author 1002821333
  */
 public class Persona {
+    
+    public String id, nombre, apellido;
+    public Producto objProducto;
+
+    public Persona() {
+    }
+
+    public Persona(String id, String nombre, String apellido, Producto objProducto) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.objProducto = objProducto;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Producto getObjProducto() {
+        return objProducto;
+    }
+
+    public void setObjProducto(Producto objProducto) {
+        this.objProducto = objProducto;
+    }
+    
     
 }
