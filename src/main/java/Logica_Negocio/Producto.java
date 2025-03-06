cc4bfe9678ef79a6d550f904bf8fcd1225cfa0a7
sandr <sandr@192.168.246.145>
2025-03-06 08:58:52 -0500

    Añadiendo clase producto

--- src/main/java/Logica_Negocio/Producto.java
+++ src/main/java/Logica_Negocio/Producto.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Logica_Negocio;
 
 /**
@@ -9,5 +6,42 @@ package Logica_Negocio;
  * @author 1002821333
  */
 public class Producto {
+    
+     public String id, nombre, descripcion;
+
+    public Producto() {
+    }
+     
+
+    public Producto(String id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+     
     
 }
